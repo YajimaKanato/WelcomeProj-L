16ebde794e2f2f32c07de8a67cdeac70564bc55a
takihito0302 <t.akihito0302@gmail.com>
2026-05-18 16:33:43 +0900

    aaaを2に変更

--- Assets/Scripts/Test.cs
+++ Assets/Scripts/Test.cs
@@ -2,7 +2,7 @@ using UnityEngine;
 
 public class Test : MonoBehaviour
 {
-    [SerializeField] private int _aaa = 1;
+    [SerializeField] private int _aaa = 2;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
