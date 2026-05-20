9c3d162c1208e54acaca0294c80cee77d09f832e
KNTy1018 <soramametaro1018@gmail.com>
2026-05-20 12:32:45 +0900

    [add]ミノの状態を定義した列挙型を作成

---
+++ Assets/Scripts/MinoDirection.cs
@@ -0,0 +1,9 @@
+using UnityEngine;
+
+public enum MinoDirection
+{
+    [InspectorName("上向き")] Up,
+    [InspectorName("下向き")] Down,
+    [InspectorName("左向き")] Left,
+    [InspectorName("右向き")] Right
+}
