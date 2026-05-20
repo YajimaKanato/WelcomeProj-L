52a2f83b481810d921243fdfa48e42de1d06e61f
KNTy1018 <soramametaro1018@gmail.com>
2026-05-20 17:16:10 +0900

    [update]簡易ランキング表示

--- Assets/Scripts/Ranking.cs
+++ Assets/Scripts/Ranking.cs
@@ -4,8 +4,17 @@ using UnityEngine;
 public class Ranking : MonoBehaviour
 {
     [SerializeField] TextMeshProUGUI[] _scores;
+    private void Start()
+    {
+        SetRanking();
+    }
 
     public void SetRanking()
     {
+        var ranking = ScoreManager.Instance.Ranking;
+        for (int i = 0; i < ranking.Count; i++)
+        {
+            _scores[i].text = (i + 1) + " : " + ranking[i].ToString("00000");
+        }
     }
 }
