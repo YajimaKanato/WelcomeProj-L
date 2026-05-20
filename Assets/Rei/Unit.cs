abe6ace259103b4758372ebdfd07b7b474c93699
mayarei2662 <mayarei2662@gmail.com>
2026-05-20 17:40:22 +0900

    [Fix]一部移動

--- Assets/Rei/Unit.cs
+++ Assets/Rei/Unit.cs
@@ -4,27 +4,26 @@ using UnityEngine;
 [CreateAssetMenu(fileName = "Unit", menuName = "Unit/Unit")]
 public class Unit : ScriptableObject
 {
-   
+    [Header("画像")]
+    [SerializeField] private Sprite _unitSprite;
     [Header("自分の設定")]
     [SerializeField] private ConditionSetting _condition;
     [Header("対応するミノの設定")]
     [SerializeField] private CorrespondSetting _unitSetting;
-  
+
+    public Sprite UnitSprite => _unitSprite;
     public ConditionSetting Conditions => _condition;
     public CorrespondSetting UnitSetting => _unitSetting;
 }
 
 [Serializable]
 public class ConditionSetting
-{
-    [Header("画像")]
-    [SerializeField] private Sprite _unitSprite;
+{ 
     [Header("自分の状態")]
     [SerializeField] private MinoDirection _myDirection;
     [Header("自分の形")]
     [SerializeField] private MinoType _myType;
 
-    public Sprite UnitSprite => _unitSprite;
     public MinoDirection MyDirection => _myDirection;
     public MinoType Type => _myType;
 }
