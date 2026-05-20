d847c60125ef13defcb36ce891aefdb9cd054aee
mayarei2662 <mayarei2662@gmail.com>
2026-05-20 17:32:40 +0900

    [Add]ユニットの対応表

---
+++ Assets/Rei/Unit.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "Unit", menuName = "Unit/Unit")]
+public class Unit : ScriptableObject
+{
+   
+    [Header("自分の設定")]
+    [SerializeField] private ConditionSetting _condition;
+    [Header("対応するミノの設定")]
+    [SerializeField] private CorrespondSetting _unitSetting;
+  
+    public ConditionSetting Conditions => _condition;
+    public CorrespondSetting UnitSetting => _unitSetting;
+}
+
+[Serializable]
+public class ConditionSetting
+{
+    [Header("画像")]
+    [SerializeField] private Sprite _unitSprite;
+    [Header("自分の状態")]
+    [SerializeField] private MinoDirection _myDirection;
+    [Header("自分の形")]
+    [SerializeField] private MinoType _myType;
+
+    public Sprite UnitSprite => _unitSprite;
+    public MinoDirection MyDirection => _myDirection;
+    public MinoType Type => _myType;
+}
+
+[Serializable]
+public class CorrespondSetting
+{
+    [Header("対応する状態")]
+    [SerializeField] private MinoDirection _correspondDirection;
+    [Header("対応する形")]
+    [SerializeField] private MinoType _correspondType;
+
+    public MinoDirection CorrespondDirection => _correspondDirection;
+
+    public MinoType Type => _correspondType;
+}
---
+++ Assets/Rei/UnitList.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+
+[CreateAssetMenu(fileName = "UnitList", menuName = "Unit/UnitList")]
+public class UnitList : ScriptableObject
+{
+    [Header("UnitList")]
+    [SerializeField] private Unit[] _unitList;
+
+    public Unit[] UnitsList => _unitList;
+}
