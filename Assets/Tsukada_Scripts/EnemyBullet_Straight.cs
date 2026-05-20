e67df85d34afd6460849c3e74286ddf39327ca34
takihito0302 <t.akihito0302@gmail.com>
2026-05-20 15:38:52 +0900

    敵の弾幕攻撃の挙動(ストレート、ウェーブ、追従)を作成

---
+++ Assets/Tsukada_Scripts/EnemyBullet_Straight.cs
@@ -0,0 +1,27 @@
+using UnityEngine;
+using DG.Tweening;
+
+public class EnemyBullet_Straight : EnemyBullet_Base
+{
+    void Start()
+    {
+        BulletMovement();
+    }
+
+    void Update()
+    {
+       
+    }
+
+
+    public override void BulletMovement()
+    {
+        transform.DOLocalMoveX(-10f, 1f)
+            .SetEase(Ease.Linear)
+           .SetLoops(-1, LoopType.Incremental);
+
+        Debug.Log("移動");
+    }
+
+    
+}
