45e25b31777d18d5dffeb3ba8068328793bb45bb
T-TaIgavgt <tsujitaiga26vantan@gmail.com>
2026-05-21 14:04:34 +0900

    bullet エフェクト

--- Assets/Scripts/chiharu tama/PlayerBulletMove.cs
+++ Assets/Scripts/chiharu tama/PlayerBulletMove.cs
@@ -6,6 +6,7 @@ public class PlayerBulletMove : MonoBehaviour
     [SerializeField] private string _tag;
     [Header("UnitList")]
     [SerializeField] private UnitList _unitlist;
+    [SerializeField] private GameObject hitEffectPrefab;
 
     private float playerBulletSpeed = 0.05f;   // 弾のスピードを決める変数
     private Renderer _renderer;
@@ -45,6 +46,7 @@ public class PlayerBulletMove : MonoBehaviour
         if (collision.gameObject.tag == _tag)
         {
             Pool.Instance.Release(this.gameObject);
+            Instantiate(hitEffectPrefab, transform.position, Quaternion.identity);
         }
     }
 }
